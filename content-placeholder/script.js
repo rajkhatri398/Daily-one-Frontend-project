c57c051789da47c9216757b7dbb4e84fd1ef152c
Raj Khatri <157197877+rajkhatri398@users.noreply.github.com>
2025-07-04 19:49:36 +0530

    Update script.js

--- content-placeholder/script.js
+++ content-placeholder/script.js
@@ -10,7 +10,7 @@ const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
 
 setTimeout(getData, 2500)
 
-function getData() {
+ getData() {
   header.innerHTML =
     '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="" />'
   title.innerHTML = 'Lorem ipsum dolor sit amet'
